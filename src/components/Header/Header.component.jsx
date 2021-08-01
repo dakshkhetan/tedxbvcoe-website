a623ed82cac9af3283f9017851bdc4cd869a3a0d
Daksh Khetan <2012daksh@gmail.com>
2021-08-02 00:32:36 +0530

    fix CI warning

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+// import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,10 +3,10 @@ import ReactGA from 'react-ga';
 
 import Hero from '../../components/Hero/Hero.component';
 import AboutBVCOE from '../../components/AboutBVCOE/AboutBVCOE.component';
-import Sponsors from '../../components/Sponsors/Sponsors.component';
-import Speakers from '../../components/Speakers/Speakers.component';
+// import Sponsors from '../../components/Sponsors/Sponsors.component';
+// import Speakers from '../../components/Speakers/Speakers.component';
 import SpeakerForm from '../../components/SpeakerForm/SpeakerForm.component';
-import SponsorUs from '../../components/SponsorUs/SponsorUs.component';
+// import SponsorUs from '../../components/SponsorUs/SponsorUs.component';
 import FAQs from '../../components/FAQs/FAQs.component';
 import Team from '../../components/Team/Team.component';
 import Contact from '../../components/Contact/Contact.component';
