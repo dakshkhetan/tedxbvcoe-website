43df509828ec43aaf7c8d479990784d963e7dd56
Daksh Khetan <2012daksh@gmail.com>
2021-08-02 00:16:55 +0530

    update team details

--- src/data/team.data.js
+++ src/data/team.data.js
@@ -3,74 +3,110 @@ export const TeamData = {
     {
       key: '1',
       name: 'Akshat Jain',
-      position: 'Fest Coordinator',
-      linkedInURL: 'https://linkedin.com/in/akshat-jain-019740170',
+      position: 'Organizer',
+      linkedInURL: 'https://linkedin.com/in/akijain2000',
       instagramURL: 'https://instagram.com/akijain2000',
-      twitterURL: 'https://twitter.com/akijain2000',
-      displayPicSrc: require('../assets/team/akshat.jpeg').default
+      twitterURL: '',
+      displayPicSrc: require('../assets/team/akshat.jpg').default
     },
     {
-      key: '2',
-      name: 'Pulkit Sharma',
-      position: 'Fest Coordinator',
-      linkedInURL: 'https://linkedin.com/in/pulkitsharma153',
-      instagramURL: 'https://instagram.com/pukki.sharma',
+      key: '8',
+      name: 'Chaitanya Chhabra',
+      position: 'Co Organizer',
+      linkedInURL: 'https://linkedin.com/in/chaitanya-chhabra',
+      instagramURL: 'https://instagram.com/chaitanya1609',
       twitterURL: '',
-      displayPicSrc: require('../assets/team/pulkit.jpeg').default
+      displayPicSrc: require('../assets/team/chaitanya.jpg').default
+    },
+    {
+      key: '11',
+      name: 'Sarthak Sethi',
+      position: 'Curator',
+      linkedInURL: 'https://linkedin.com/in/sarthak-sethi-8497b5174',
+      instagramURL: 'https://instagram.com/sarthak_sethi6',
+      twitterURL: 'https://twitter.com/sarthak_sethi6',
+      displayPicSrc: require('../assets/team/sarthak_sethi.jpg').default
     },
     {
       key: '3',
       name: 'Daksh Khetan',
-      position: 'Lead Developer',
+      position: 'Technical Head',
       linkedInURL: 'https://linkedin.com/in/dakshkhetan',
       instagramURL: 'https://instagram.com/dakshkhetan',
       twitterURL: 'https://twitter.com/dakshkhetan',
-      displayPicSrc: require('../assets/team/daksh.jpeg').default
-    },
-    {
-      key: '4',
-      name: 'Sanjay Singh',
-      position: 'Design Head',
-      linkedInURL: 'https://linkedin.com/in/sanjay-singh-b84aa7137',
-      instagramURL: 'https://instagram.com/oye.sanjay',
-      twitterURL: 'https://twitter.com/oye_sanjaay',
-      displayPicSrc: require('../assets/team/sanjay.jpeg').default
-    },
-    {
-      key: '5',
-      name: 'Mishael Thomas',
-      position: 'Event Management',
-      linkedInURL: 'https://linkedin.com/in/mishaelthomas1482',
-      instagramURL: 'https://instagram.com/mishael1482',
-      twitterURL: '',
-      displayPicSrc: require('../assets/team/mishael.jpeg').default
+      displayPicSrc: require('../assets/team/daksh.jpg').default
     },
     {
       key: '6',
-      name: 'Ankita Natarajan',
-      position: 'Documentation',
-      linkedInURL: 'https://linkedin.com/in/ankita-natarajan-aa5079170',
-      instagramURL: 'https://instagram.com/ankita624',
-      twitterURL: 'https://twitter.com/ankitanatarajan',
-      displayPicSrc: require('../assets/team/ankita.jpeg').default
+      name: 'Piyush Pant',
+      position: 'Documentation Head',
+      linkedInURL: 'https://linkedin.com/in/piyush-pant-37b16a191',
+      instagramURL: '',
+      twitterURL: '',
+      displayPicSrc: require('../assets/team/piyush.jpg').default
     },
     {
       key: '7',
-      name: 'Prerna Arya',
-      position: 'Sponsorship',
-      linkedInURL: 'https://linkedin.com/in/prerna-arya-a1a8a9179',
-      instagramURL: 'https://instagram.com/aryaprerna23',
+      name: 'Siddhant Jha',
+      position: 'Creativity & Design Head',
+      linkedInURL: 'https://linkedin.com/in/siddhant-jha-8a9366191',
+      instagramURL: 'https://instagram.com/siddhant_2602',
       twitterURL: '',
-      displayPicSrc: require('../assets/team/prerna.jpeg').default
+      displayPicSrc: require('../assets/team/siddhant.jpg').default
     },
     {
-      key: '8',
-      name: 'Keshav Goyal',
-      position: 'Social Media',
-      linkedInURL: 'https://linkedin.com/in/keshav-goyal-768302174',
-      instagramURL: 'https://instagram.com/_keshav_goyal',
+      key: '9',
+      name: 'Ritika Yadav',
+      position: 'Joint Social Media Head',
+      linkedInURL: 'https://linkedin.com/in/ritika-yadav-3184811b8',
+      instagramURL: 'https://instagram.com/riitika_yadav',
+      twitterURL: 'https://twitter.com/Ritika21199418',
+      displayPicSrc: require('../assets/team/ritika.jpg').default
+    },
+    {
+      key: '10',
+      name: 'Aditi Sharma',
+      position: 'Core Member',
+      linkedInURL: 'https://linkedin.com/in/aditi-sharma-7a5bab1aa',
+      instagramURL: 'https://instagram.com/_.aadeeti._',
       twitterURL: '',
-      displayPicSrc: require('../assets/team/keshav.jpeg').default
+      displayPicSrc: require('../assets/team/aditi.jpg').default
+    },
+    {
+      key: '12',
+      name: 'Sarthak Gautam',
+      position: 'Events Head',
+      linkedInURL: 'https://linkedin.com/in/sarthak-gautam-5a5a7a1a0',
+      instagramURL: 'https://instagram.com/_sarthakgautam_',
+      twitterURL: '',
+      displayPicSrc: require('../assets/team/sarthak_gautam.jpg').default
+    },
+    {
+      key: '4',
+      name: 'Ankita Natarajan',
+      position: 'Communication & Outreach Head',
+      linkedInURL: 'https://linkedin.com/in/ankita-natarajan',
+      instagramURL: 'https://instagram.com/ankita624',
+      twitterURL: '',
+      displayPicSrc: require('../assets/team/ankita.jpg').default
     }
+    // {
+    //   key: '2',
+    //   name: 'Pulkit Sharma',
+    //   position: 'Co Organizer',
+    //   linkedInURL: 'https://linkedin.com/in/pulkitsharma153',
+    //   instagramURL: 'https://instagram.com/pukki.sharma',
+    //   twitterURL: '',
+    //   displayPicSrc: require('../assets/team/pulkit.jpg').default
+    // },
+    // {
+    //   key: '13',
+    //   name: 'sanjay',
+    //   position: 'Design',
+    //   linkedInURL: 'https://linkedin.com/in/sanjay',
+    //   instagramURL: 'https://instagram.com/sanjay',
+    //   twitterURL: '',
+    //   displayPicSrc: require('../assets/team/sanjay.jpg').default
+    // }
   ]
 };
