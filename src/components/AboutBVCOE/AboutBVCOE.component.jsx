c3403742c57f1296a6aa23d31950cdee40edc486
Daksh Khetan <2012daksh@gmail.com>
2021-08-01 21:47:54 +0530

    update 'About BVCOE' section

--- src/components/AboutBVCOE/AboutBVCOE.component.jsx
+++ src/components/AboutBVCOE/AboutBVCOE.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Fade from 'react-reveal/Fade';
 import Zoom from 'react-reveal/Zoom';
 
-import placeholder from '../../assets/about-section/placeholder.png';
+import BVCOEImage from '../../assets/about-section/bvcoe.png';
 
 import './AboutBVCOE.styles.scss';
 
@@ -11,24 +11,40 @@ const AboutBVCOE = () => (
     <Fade bottom delay={100}>
       <h1 className='section-heading'>About BVCOE</h1>
     </Fade>
+
     <div className='section-content'>
+      <div className='image-container'>
+        <Zoom delay={200}>
+          <img src={BVCOEImage} alt='' />
+        </Zoom>
+      </div>
+
       <div className='info'>
         <Fade bottom delay={150}>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            Bharati Vidyapeeth's College of Engineering, New Delhi since its
+            establishment in 1999, has strived to provide the best engineering
+            education, its students, through well-qualified and dedicated
+            faculty. The college is affiliated with Guru Gobind Singh
+            Indraprastha University (GGSIPU), New Delhi, and is considered one
+            of the leading institutes for engineering in the city. Currently,
+            BVCOE offers various courses like B Tech, BBA, BCA, MBA, MCA. The
+            college consists of many technical and non-technical societies like
+            BVPIEEE, ISTE, ACM, CSI, Enactus, Eduminerva, DAS, EDC, Zaahir, etc.
+            Students working/volunteering under these societies are very active
+            in social and cultural events and are readily involved in social or
+            educational volunteering across the city.
+          </p>
+          <p>
+            <u>VISION</u>: The vision of the college is to continuously exceed
+            and thus coming together enlivens the research themes, creates
+            awareness about upcoming technologies, and provides a platform to
+            budding research workers for producing their significant place in
+            the scientific community. Not only that, the premium institute
+            focuses on the overall development of their students and faculties
+            by having various technical and social gatherings all year round.
           </p>
         </Fade>
-      </div>
-      <div className='image-container'>
-        <Zoom delay={200}>
-          <img src={placeholder} alt='' />
-        </Zoom>
       </div>
     </div>
   </section>
