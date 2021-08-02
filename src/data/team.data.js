d7d8769b6e179d282d15e242a101cb632a88f25a
Daksh Khetan <2012daksh@gmail.com>
2021-08-02 22:33:08 +0530

    update team details

--- src/data/team.data.js
+++ src/data/team.data.js
@@ -10,7 +10,7 @@ export const TeamData = {
       displayPicSrc: require('../assets/team/akshat.jpg').default
     },
     {
-      key: '8',
+      key: '2',
       name: 'Chaitanya Chhabra',
       position: 'Co Organizer',
       linkedInURL: 'https://linkedin.com/in/chaitanya-chhabra',
@@ -19,7 +19,16 @@ export const TeamData = {
       displayPicSrc: require('../assets/team/chaitanya.jpg').default
     },
     {
-      key: '11',
+      key: '3',
+      name: 'Prerna Arya',
+      position: 'Core Team'
+      // linkedInURL: 'https://linkedin.com/in/prerna',
+      // instagramURL: 'https://instagram.com/prerna',
+      // twitterURL: '',
+      // displayPicSrc: require('../assets/team/prerna.jpg').default
+    },
+    {
+      key: '4',
       name: 'Sarthak Sethi',
       position: 'Curator',
       linkedInURL: 'https://linkedin.com/in/sarthak-sethi-8497b5174',
@@ -28,52 +37,52 @@ export const TeamData = {
       displayPicSrc: require('../assets/team/sarthak_sethi.jpg').default
     },
     {
-      key: '3',
-      name: 'Daksh Khetan',
-      position: 'Technical Head',
-      linkedInURL: 'https://linkedin.com/in/dakshkhetan',
-      instagramURL: 'https://instagram.com/dakshkhetan',
-      twitterURL: 'https://twitter.com/dakshkhetan',
-      displayPicSrc: require('../assets/team/daksh.jpg').default
+      key: '5',
+      name: 'Parinika Singh',
+      position: 'Core Team'
+      // linkedInURL: 'https://linkedin.com/in/parinika',
+      // instagramURL: 'https://instagram.com/parinika',
+      // twitterURL: '',
+      // displayPicSrc: require('../assets/team/parinika.jpg').default
     },
     {
       key: '6',
-      name: 'Piyush Pant',
-      position: 'Documentation Head',
-      linkedInURL: 'https://linkedin.com/in/piyush-pant-37b16a191',
-      instagramURL: '',
+      name: 'Pulkit Sharma',
+      position: 'Core Team',
+      linkedInURL: 'https://linkedin.com/in/pulkitsharma153',
+      instagramURL: 'https://instagram.com/pulk1tsharma',
       twitterURL: '',
-      displayPicSrc: require('../assets/team/piyush.jpg').default
+      displayPicSrc: require('../assets/team/pulkit.jpg').default
     },
     {
       key: '7',
-      name: 'Siddhant Jha',
-      position: 'Creativity & Design Head',
-      linkedInURL: 'https://linkedin.com/in/siddhant-jha-8a9366191',
-      instagramURL: 'https://instagram.com/siddhant_2602',
+      name: 'Khushboo',
+      position: 'Core Team',
+      linkedInURL: 'https://linkedin.com/in/khushboo-sharma-347577170',
+      instagramURL: 'https://instagram.com/simplyme2400',
       twitterURL: '',
-      displayPicSrc: require('../assets/team/siddhant.jpg').default
+      displayPicSrc: require('../assets/team/khushboo.jpg').default
+    },
+    {
+      key: '8',
+      name: 'Drishti Hans',
+      position: 'Core Team'
+      // linkedInURL: 'https://linkedin.com/in/drishti',
+      // instagramURL: 'https://instagram.com/drishti',
+      // twitterURL: '',
+      // displayPicSrc: require('../assets/team/drishti.jpg').default
     },
     {
       key: '9',
-      name: 'Ritika Yadav',
-      position: 'Joint Social Media Head',
-      linkedInURL: 'https://linkedin.com/in/ritika-yadav-3184811b8',
-      instagramURL: 'https://instagram.com/riitika_yadav',
-      twitterURL: 'https://twitter.com/Ritika21199418',
-      displayPicSrc: require('../assets/team/ritika.jpg').default
-    },
-    {
-      key: '10',
       name: 'Aditi Sharma',
-      position: 'Core Member',
+      position: 'Core Team',
       linkedInURL: 'https://linkedin.com/in/aditi-sharma-7a5bab1aa',
       instagramURL: 'https://instagram.com/_.aadeeti._',
       twitterURL: '',
       displayPicSrc: require('../assets/team/aditi.jpg').default
     },
     {
-      key: '12',
+      key: '10',
       name: 'Sarthak Gautam',
       position: 'Events Head',
       linkedInURL: 'https://linkedin.com/in/sarthak-gautam-5a5a7a1a0',
@@ -82,31 +91,76 @@ export const TeamData = {
       displayPicSrc: require('../assets/team/sarthak_gautam.jpg').default
     },
     {
-      key: '4',
+      key: '11',
       name: 'Ankita Natarajan',
       position: 'Communication & Outreach Head',
       linkedInURL: 'https://linkedin.com/in/ankita-natarajan',
       instagramURL: 'https://instagram.com/ankita624',
       twitterURL: '',
       displayPicSrc: require('../assets/team/ankita.jpg').default
+    },
+    {
+      key: '12',
+      name: 'Siddhant Jha',
+      position: 'Creativity & Design Head',
+      linkedInURL: 'https://linkedin.com/in/siddhant-jha-8a9366191',
+      instagramURL: 'https://instagram.com/siddhant_2602',
+      twitterURL: '',
+      displayPicSrc: require('../assets/team/siddhant.jpg').default
+    },
+    {
+      key: '13',
+      name: 'Daksh Khetan',
+      position: 'Technical Head',
+      linkedInURL: 'https://linkedin.com/in/dakshkhetan',
+      instagramURL: 'https://instagram.com/dakshkhetan',
+      twitterURL: 'https://twitter.com/dakshkhetan',
+      displayPicSrc: require('../assets/team/daksh.jpg').default
+    },
+    {
+      key: '14',
+      name: 'Uday Jain',
+      position: 'Technical Head'
+      // linkedInURL: 'https://linkedin.com/in/uday',
+      // instagramURL: 'https://instagram.com/uday',
+      // twitterURL: '',
+      // displayPicSrc: require('../assets/team/uday.jpg').default
+    },
+    {
+      key: '15',
+      name: 'Piyush Pant',
+      position: 'Documentation Head',
+      linkedInURL: 'https://linkedin.com/in/piyush-pant-37b16a191',
+      instagramURL: '',
+      twitterURL: '',
+      displayPicSrc: require('../assets/team/piyush.jpg').default
+    },
+    {
+      key: '16',
+      name: 'Rishab',
+      position: 'Event Coverage'
+      // linkedInURL: 'https://linkedin.com/in/rishab',
+      // instagramURL: 'https://instagram.com/rishab',
+      // twitterURL: '',
+      // displayPicSrc: require('../assets/team/rishab.jpg').default
+    },
+    {
+      key: '17',
+      name: 'Aastha Suri',
+      position: 'Social Media & Publicity Head',
+      linkedInURL: 'https://linkedin.com/in/aastha-suri-557274181',
+      instagramURL: 'https://instagram.com/aastha402',
+      twitterURL: '',
+      displayPicSrc: require('../assets/team/aastha.jpg').default
+    },
+    {
+      key: '18',
+      name: 'Ritika Yadav',
+      position: 'Joint Social Media Head',
+      linkedInURL: 'https://linkedin.com/in/ritika-yadav-3184811b8',
+      instagramURL: 'https://instagram.com/riitika_yadav',
+      twitterURL: 'https://twitter.com/Ritika21199418',
+      displayPicSrc: require('../assets/team/ritika.jpg').default
     }
-    // {
-    //   key: '2',
-    //   name: 'Pulkit Sharma',
-    //   position: 'Co Organizer',
-    //   linkedInURL: 'https://linkedin.com/in/pulkitsharma153',
-    //   instagramURL: 'https://instagram.com/pukki.sharma',
-    //   twitterURL: '',
-    //   displayPicSrc: require('../assets/team/pulkit.jpg').default
-    // },
-    // {
-    //   key: '13',
-    //   name: 'sanjay',
-    //   position: 'Design',
-    //   linkedInURL: 'https://linkedin.com/in/sanjay',
-    //   instagramURL: 'https://instagram.com/sanjay',
-    //   twitterURL: '',
-    //   displayPicSrc: require('../assets/team/sanjay.jpg').default
-    // }
   ]
 };
